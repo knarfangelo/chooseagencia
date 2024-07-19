597a825d0fde673bd4e0a4f573e1fe346d02539b
knarfangelo <knarf2003angelo@gmail.com>
2024-07-19 12:30:03 -0500

    Corrigiendo el panel principal

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -12,7 +12,7 @@ import { MaquinaEscribirComponent } from "./maquina-escribir/maquina-escribir.co
     MaquinaEscribirComponent
   ],
   template: `
-    <header 
+<header 
       (mousemove)="actualizarPosicionLupa($event)" 
       (mouseleave)="ocultarLupa()"
       (mousedown)="activarLupa($event)" 
@@ -40,11 +40,18 @@ export class InicioComponent {
   lupaHeight = 200; 
   lupaHidden = true;
   isMouseDown = false;
+  isMobile = false; // Variable para detectar dispositivos móviles
 
-  constructor(private renderer: Renderer2) {}
+  constructor(private renderer: Renderer2) {
+    this.detectarDispositivoMovil();
+  }
+
+  detectarDispositivoMovil() {
+    this.isMobile = window.innerWidth <= 800;
+  }
 
   actualizarPosicionLupa(event: MouseEvent | TouchEvent) {
-    if (!this.lupaHidden || this.isMouseDown) {
+    if (!this.lupaHidden && (this.isMobile || this.isMouseDown)) {
       // Obtener las coordenadas del evento
       let clientX: number = 0;
       let clientY: number = 0;
@@ -57,15 +64,15 @@ export class InicioComponent {
         clientX = touch.clientX;
         clientY = touch.clientY;
       }
-  
+
       // Ajustar por el desplazamiento de la página
       const offsetX = this.lupaWidth / 2;
       const offsetY = this.lupaHeight / 2;
-      
+
       // Obtener las coordenadas ajustadas con el scroll
       const left = clientX + window.scrollX - offsetX;
       const top = clientY + window.scrollY - offsetY;
-      
+
       // Actualizar la posición de la lupa usando Renderer2
       this.renderer.setStyle(this.lupa?.nativeElement, 'left', `${left}px`);
       this.renderer.setStyle(this.lupa?.nativeElement, 'top', `${top}px`);
@@ -81,16 +88,22 @@ export class InicioComponent {
   }
 
   mostrarLupa() {
-    this.lupaHidden = false;
+    if (this.isMobile && this.isMouseDown) {
+      this.lupaHidden = false;
+    }
   }
 
   activarLupa(event: MouseEvent | TouchEvent) {
     this.isMouseDown = true;
-    this.lupaHidden = false;
-    this.actualizarPosicionLupa(event);
+    if (this.isMobile) {
+      this.mostrarLupa(); // Mostrar la lupa solo en dispositivos móviles al mantener presionado
+    } else {
+      this.actualizarPosicionLupa(event); // Actualizar la posición en otros dispositivos al activarse
+    }
   }
 
   desactivarLupa() {
     this.isMouseDown = false;
+    this.lupaHidden = true; // Ocultar la lupa al dejar de presionar
   }
 }
