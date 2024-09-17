3028c73054f0167b5c4c14c1c433c7bf11f8720e
knarfangelo <knarf2003angelo@gmail.com>
2024-09-17 12:37:11 -0500

    responsive

--- src/app/components/trabaja-nosotros/trabaja-nosotros.component.ts
+++ src/app/components/trabaja-nosotros/trabaja-nosotros.component.ts
@@ -13,25 +13,25 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
      <main>
       <form action="">
         <label for=""><p>Nombres<span>*</span></p>
-          <input type="text" name="" id="">
+          <input type="text" name="" id="" placeholder="Tus nombres">
         </label>
         <label for=""><p>Apellidos<span>*</span></p>
-          <input type="text" name="" id="">
+          <input type="text" name="" id="" placeholder="Tus apellidos">
         </label>
         <label for=""><p>Correo electronico<span>*</span></p>
-          <input type="text" name="" id="">
+          <input type="text" name="" id="" placeholder="ejemplo@gmail.com">
         </label>
         <label for=""><p>Celular</p>
-          <input type="text" name="" id="">
+          <input type="text" name="" id="" placeholder="952599717">
         </label>
         <label for=""><p>Linkedin</p>
-          <input type="text" name="" id="">
+          <input type="text" name="" id="" placeholder="Tu linkedin">
         </label>
         <label for=""><p>CV</p>
-          <input type="text" name="" id="">
+          <input type="text" name="" id="" placeholder="Adjunta tu cv">
         </label>
         <label for=""><p>Portafolio</p>
-          <input type="text" name="" id="">
+          <input type="text" name="" id="" placeholder="Adjunta tu portafolio">
         </label>
       </form>
       <img class="charles" src="trabaja-nosotros/charles.png" alt=""> </main>
