179c09d31bb6b11af75a49a0760e2bed3bc14bb1
knarfangelo <knarf2003angelo@gmail.com>
2024-10-30 16:16:13 -0500

    lmscaaa1asvp

--- src/app/components/VoiceflowComponent/VoiceflowComponent.component.ts
+++ src/app/components/VoiceflowComponent/VoiceflowComponent.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule, isPlatformBrowser } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Inject, PLATFORM_ID, AfterViewInit, HostListener } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, PLATFORM_ID, AfterViewInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-voiceflow-component',
@@ -8,30 +8,41 @@ import { ChangeDetectionStrategy, Component, Inject, PLATFORM_ID, AfterViewInit,
     CommonModule,
   ],
   template: `
-  <div class="bot">
-  <div id="voiceflow-container"></div></div>`,
+    <div id="voiceflow-container" style="position: relative; height: 500px;"></div>
+  `,
   styleUrls: ['./VoiceflowComponent.component.css'],
 })
 export class VoiceflowComponentComponent implements AfterViewInit {
-
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  constructor(@Inject(PLATFORM_ID) private platformId: Object, private renderer: Renderer2) {}
 
   ngAfterViewInit() {
     if (isPlatformBrowser(this.platformId)) {
-      const script = document.createElement('script');
+      const script = this.renderer.createElement('script');
       script.src = 'https://cdn.voiceflow.com/widget/bundle.mjs';
       script.type = 'text/javascript';
       script.onload = () => {
+        // Cargar el bot de Voiceflow
         (window as any).voiceflow.chat.load({
-          verify: { projectID: '66a2aadb5515f333bd5e9bd6' },
+          verify: { projectID: '66a2aadb5515f333bd5e9bd6' }, // Reemplaza 'PROJECT_ID' con tu ID de proyecto
           url: 'https://general-runtime.voiceflow.com',
           versionID: 'production',
+          assistant: {
+            stylesheet: 'styles.css'
+          }
+        }).then(() => {
+          // Limpiar mensajes anteriores
+          (window as any).voiceflow.chat.proactive.clear();
+
+          // Agregar mensajes proactivos
+          (window as any).voiceflow.chat.proactive.push(
+            { 
+              type: 'text', 
+              payload: { message: 'Hola, Soy el bot de Choose' } 
+            }, 
+          );
         });
       };
-      document.getElementById('voiceflow-container')?.appendChild(script);
-   
-        
+      this.renderer.appendChild(document.body, script);
     }
   }
-
 }
